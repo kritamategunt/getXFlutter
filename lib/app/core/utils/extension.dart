5aa28ea1f99c8990758f5c21b14b5ebf95b02948
Kritamate <Kritamate.bs@gmail.com>
2023-04-04 14:48:03 +0700

    create extension file

---
+++ lib/app/core/utils/extension.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+extension PercentSize on double {
+  double get hp => (Get.height * (this / 100));
+  double get wp => (Get.width * (this / 100));
+}
+
+extension ResponseText on double {
+  double get sp => Get.width / 100 * (this / 3);
+}
